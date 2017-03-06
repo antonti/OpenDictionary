c7bdd781b4a6f4a8612a9e7511bad49e802b7aa0
Anton <anton.timashkov.w@gmail.com>
2017-03-06 14:09:19 +0200

    Added Web api controller to DictionaryWebAPI project. Cleaned VocabularyBuilder project.

---
+++ DictionaryWebAPI/Controllers/DictionaryController.cs
@@ -0,0 +1,30 @@
+﻿using DictionaryWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DictionaryWebAPI.Controllers
+{
+    public class DictionaryController : ApiController
+    {
+        private DictionaryContext _db = new DictionaryContext();
+
+        public IHttpActionResult GetWordInfo(string word)
+        {
+            if(string.IsNullOrEmpty(word))
+            {
+                return BadRequest();
+            }
+            var wordInfo = _db.Words.Where(w => w.word == word)?.Include(w => w.Definitions);
+            if(wordInfo == null)
+            {
+                return NotFound();
+            }
+            return Ok(wordInfo);
+        }
+    }
+}
